82e62422ae61d2ae23da416ea5c335bbbbd152f1
EmilLM <luchianemil@gmail>
2020-10-11 22:33:02 +0300

    rwd changes

--- app-portfolio/components/Content/Projects/SlickCarousel.jsx
+++ app-portfolio/components/Content/Projects/SlickCarousel.jsx
@@ -18,9 +18,17 @@ export default function SlickCarousel() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          initialSlide: 1
         }
       },
       {
