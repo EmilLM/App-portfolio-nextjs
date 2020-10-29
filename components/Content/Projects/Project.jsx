f41e68cfefec3ea1c793da68bfbc8145e195613a
EmilLM <luchianemil@gmail>
2020-10-29 15:59:03 +0200

    added nextjs lazy load

--- components/Content/Projects/Project.jsx
+++ components/Content/Projects/Project.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import LazyLoad from 'react-lazyload';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import {faLaptopCode} from '@fortawesome/free-solid-svg-icons'
 import Waves from "../../Waves"
-
+import Image from "next/image"
 
 const Project = ({project}) => {
 
@@ -13,35 +13,32 @@ const Project = ({project}) => {
 
     return ( 
         <div className="project-container">
-             <LazyLoad height={150} offset={100}>
-                <div className="content-container project">
+            <div className="content-container project">
+                <h3> &lt; {title} /&gt;</h3>
 
-                    <h3> &lt; {title} /&gt;</h3>
+                <div className="image-container">
+                    <a href={link} target="_blank">
+                        <Image className="project-image" src={`/assets/${src}`} alt={altText} unsized/>
+                    </a>
+                </div>
 
-                    <div className="image-container">
-                        <a href={link} target="_blank">
-                            <img className="project-image" src={`/assets/${src}`} alt={altText}/>
+                <div className="description">
+                    <p>{description}</p>
+                    <strong>{stack}</strong>
+
+                    <div className="project-links">
+                        <a href={link} target="_blank" rel="noopener noreferrer" id="demo">
+                            <FontAwesomeIcon icon={faLaptopCode}/>   Demo
+                        </a>   
+                        <a href={sourceLink} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGithubSquare}/>  Source
                         </a>
                     </div>
 
-                    <div className="description">
-                        <p>{description}</p>
-                        <strong>{stack}</strong>
-
-                        <div className="project-links">
-                            <a href={link} target="_blank" rel="noopener noreferrer" id="demo">
-                                <FontAwesomeIcon icon={faLaptopCode}/>   Demo
-                            </a>   
-                            <a href={sourceLink} target="_blank" rel="noopener noreferrer">
-                                <FontAwesomeIcon icon={faGithubSquare}/>  Source
-                            </a>
-                        </div>
-
-                    </div> 
-
                 </div> 
-            </LazyLoad>  
-            <Waves wavesClass={wavesClass}/>
+
+            </div> 
+        <Waves wavesClass={wavesClass}/>
         </div>
      );
 }
