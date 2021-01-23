31fa6ad089df55f08cf7020e9a6597c82eeaa828
EmilLM <luchianemil@gmail>
2021-01-23 11:42:13 +0200

    neumorphism redesign

---
+++ pages/neumorphism.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+const NeumorphismPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Apps Portfolio</title>
+            </Head>
+            
+        </>
+    );
+}
+ 
+export default NeumorphismPage;
