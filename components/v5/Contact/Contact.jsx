8e6492f3fa5f5b13b71d37f4112ba58793e67f9f
EmilLM <luchianemil@gmail>
2021-02-16 19:53:39 +0200

    contact styling

--- components/v5/Contact/Contact.jsx
+++ components/v5/Contact/Contact.jsx
@@ -1,10 +1,11 @@
+import ContactForm from './ContactForm';
 import style from './contact.module.scss';
-
 const Contact = () => {
 	return (
 		<div className={style.container}>
 			<h1 className={style.title}>Contact</h1>
-			<div className={style.contact_form}></div>
+
+			<ContactForm />
 		</div>
 	);
 };
